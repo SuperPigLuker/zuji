34e2617566bbbdbd7797fb1661bca21927051cbf
xsyatgh <xsyatwangyi@163.com>
2015-07-06 23:39:47 +0800

    Go！

---
+++ ZJ/ZJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJ
+//
+//  Created by xueshouyang on 15/7/6.
+//  Copyright © 2015年 ZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJ/ZJ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZJ
+//
+//  Created by xueshouyang on 15/7/6.
+//  Copyright © 2015年 ZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
